c4203e7305973d3a11676253ec042e5856ecae84
Ivanov Nikita <destira@mail.ru>
2023-08-04 17:52:23 +0300

    Update .github

--- orm_migrations/school/migrations/tests.py
+++ orm_migrations/school/migrations/tests.py
@@ -6,4 +6,4 @@ class TestSampleView(TestCase):
         url = 'http://194.58.103.43/students/'
         client = APIClient()
         response = client.get(url)
-        self.assertEquals(200, 200)
+        self.assertEquals(1, 1)
